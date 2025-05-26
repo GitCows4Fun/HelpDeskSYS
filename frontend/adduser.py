caf9b6582c02ee9a1a348e0ba53523d74f1ac385
DVP-F <carnx@duck.com>
2025-05-26 22:00:41 +0300

    adduser update

--- frontend/adduser.py
+++ frontend/adduser.py
@@ -1,2 +1,30 @@
-def addUser(email: str, commonName: str, password: str):email=input('Email Address: ')if not email else 0;commonName=input('Name: ')if not commonName else 0;password=input('Password: ')if not password else 0;pw_hash=__import__('hashlib').sha256(password,usedforsecurity=True);dbConf = {'host':'localhost','user':'root','password':'','database':'ticketdb'}; script = f"USE `ticketdb`;\nINSERT INTO users (commonName, email, passwordHash); VALUES (`{commonName}`, `{email}`, `{str(pw_hash)}`);";conn = __import__('mysql',fromlist=['mysql.connector']).connect(**dbConf);cur=conn.cursor();cur.execute(script);cur.close();conn.close()
-if __name__ == "__main__": argv=__import__('sys',fromlist=['argv']);print('Arguments:\temail: str\tcommonName: str\tpassword: str')if (len(argv)!=4 and len(argv)>1)else addUser(argv[1],argv[2],argv[3])if len(argv)==4 else addUser()
+# i=__import__;n=input;a=i('sys').argv;e,c,p=(a[1:4]+[""]*3)[:3];e=e or n('Email:');c=c or n('Name:');p=p or n('Pass:');w=i('hashlib').sha256(p.encode()).hexdigest();d={'host':'localhost','user':'root','database':'ticketdb'};s=f"INSERT INTO users(commonName,email,passwordHash)VALUES('{c}','{e}','{w}')";i('mysql.connector').connect(**d).cursor().execute(s).close().close()
+
+import mysql.connector; import hashlib; from sys import argv 
+
+def addUser(email='', commonName='', password=''): 
+    if not email: email = input('Email Address: ') 
+    if not commonName: commonName = input('Name: ') 
+    if not password: password = input('Password: ') 
+
+    hashobj = hashlib.sha256() 
+    hashobj.update(password.encode('utf-8')) 
+    pw_hash = hashobj.hexdigest() 
+
+    dbConf = {'host':'localhost','user':'root','password':'','database':'ticketdb'} 
+    script = f"""
+INSERT INTO users (commonName, email, passwordHash) 
+VALUES ('{commonName}', '{email}', '{pw_hash}');
+""" 
+    conn = mysql.connector.connect(**dbConf) 
+    cur = conn.cursor() 
+    cur.execute(script) 
+    cur.close(); conn.close() 
+
+if __name__ == "__main__": 
+    if (1 <= len(argv) <= 3) or len(argv) > 4: 
+        print('Usage:\temail\tcommonName\tpassword') 
+    else: 
+        if len(argv) ==4: 
+            addUser(argv[1], argv[2], argv[3]) 
+        else: addUser() 
